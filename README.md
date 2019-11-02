7c06968631370ca4a69df574db04795f54622e5d
lingyunhao <lingyunh@andrew.cmu.edu>
2019-11-02 13:12:05 -0400

    update

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Deep Learning with Differential Privacy
 
-## Installation
-
-```bash
-pip install -r requirements.txt --upgrade --force-reinstall
+## Prerequisites
+Windows 10 + CUDA 10 + CUDNN 7 + TensorFlow 2.0 with Anaconda 3
+```
+conda create -n tf2 python=3.6
+activate tf2
+conda install tensorflow-gpu==2.0.0
+pip install tensorflow-privacy==0.1.0
 ```
--- mnist.py
+++ mnist.py
@@ -1,10 +1,25 @@
-"""Training a CNN on MNIST in with DP-SGD optimizer."""
+# Copyright 2019, The TensorFlow Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Training a CNN on MNIST with Keras and the DP SGD optimizer."""
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from absl import app
 from absl import flags
+from absl import logging
 
 import numpy as np
 import tensorflow as tf
@@ -13,19 +28,19 @@ from privacy.analysis.rdp_accountant import compute_rdp
 from privacy.analysis.rdp_accountant import get_privacy_spent
 from dp_optimizer import DPGradientDescentGaussianOptimizer
 
+GradientDescentOptimizer = tf.compat.v1.train.GradientDescentOptimizer
 
 flags.DEFINE_boolean('dpsgd', True, 'If True, train with DP-SGD. If False, train with vanilla SGD.')
-flags.DEFINE_float('learning_rate', 0.15, 'Learning rate for training')
+flags.DEFINE_float('learning_rate', 1e-3, 'Learning rate for training')
 flags.DEFINE_float('noise_multiplier', 1.1, 'Ratio of the standard deviation to the clipping norm')
 flags.DEFINE_float('l2_norm_clip', 1.0, 'Clipping norm')
-flags.DEFINE_integer('batch_size', 100, 'Batch size')
-flags.DEFINE_integer('epochs', 60, 'Number of epochs')
-flags.DEFINE_integer('microbatches', 8, 'Number of microbatches (must evenly divide batch_size)')
+flags.DEFINE_integer('batch_size', 250, 'Batch size')
+flags.DEFINE_integer('epochs', 400, 'Number of epochs')
+flags.DEFINE_integer('microbatches', 250, 'Number of microbatches (must evenly divide batch_size)')
+flags.DEFINE_string('model_dir', None, 'Model directory')
 
 FLAGS = flags.FLAGS
-
-# Delta is set to 1e-5 because MNIST has 60000 training points.
-delta=1e-5
+delta = 1e-2  # Delta is set to 1e-5 because MNIST has 60000 training points.
 
 
 def compute_epsilon(steps):
@@ -38,88 +53,81 @@ def compute_epsilon(steps):
   return get_privacy_spent(orders, rdp, target_delta=delta)[0]
 
 
-def main(_):
+class EpsilonPrintingCallback(tf.keras.callbacks.Callback):
+  """Callback for Keras model to evaluate epsilon after every epoch."""
+  def __init__(self):
+    self.eps_history = []
+
+  def on_epoch_end(self, epoch, logs=None):
+    if FLAGS.dpsgd:
+      eps = compute_epsilon((epoch + 1) * (60000 // FLAGS.batch_size))
+      self.eps_history.append(eps)
+      print(', eps = {}'.format(eps))
+
+
+def load_mnist():
+  """Loads MNIST and preprocesses to combine training and validation data."""
+  train, test = tf.keras.datasets.mnist.load_data()
+  train_data, train_labels = train
+  test_data, test_labels = test
+
+  train_data = np.array(train_data, dtype=np.float32) / 255
+  test_data = np.array(test_data, dtype=np.float32) / 255
+
+  train_data = train_data.reshape(train_data.shape[0], 28, 28, 1)
+  test_data = test_data.reshape(test_data.shape[0], 28, 28, 1)
+
+  train_labels = np.array(train_labels, dtype=np.int32)
+  test_labels = np.array(test_labels, dtype=np.int32)
+
+  train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=10)
+  test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=10)
+
+  return train_data, train_labels, test_data, test_labels
+
+
+def main(unused_argv):
+  logging.set_verbosity(logging.INFO)
   if FLAGS.dpsgd and FLAGS.batch_size % FLAGS.microbatches != 0:
     raise ValueError('Number of microbatches should divide evenly batch_size')
 
-  # Fetch the mnist data
-  train, test = tf.keras.datasets.mnist.load_data()
-  train_images, train_labels = train
-  test_images, test_labels = test
+  # Load training and test data.
+  train_data, train_labels, test_data, test_labels = load_mnist()
 
-  # Create a dataset object and batch for the training data
-  dataset = tf.data.Dataset.from_tensor_slices(
-      (tf.cast(train_images[..., tf.newaxis]/255, tf.float32),
-       tf.cast(train_labels, tf.int64)))
-  dataset = dataset.shuffle(1000).batch(FLAGS.batch_size)
-
-  # Create a dataset object and batch for the test data
-  eval_dataset = tf.data.Dataset.from_tensor_slices(
-      (tf.cast(test_images[..., tf.newaxis]/255, tf.float32),
-       tf.cast(test_labels, tf.int64)))
-  eval_dataset = eval_dataset.batch(10000)
-
-  # Define the model using tf.keras.layers
-  mnist_model = tf.keras.Sequential([
-      tf.keras.layers.Conv2D(16, 8, strides=2, padding='same', activation='relu'),
+  # Define a sequential Keras model
+  model = tf.keras.Sequential([
+      tf.keras.layers.Conv2D(16, 8, strides=2, padding='same', activation='relu', input_shape=(28, 28, 1)),
       tf.keras.layers.MaxPool2D(2, 1),
-      tf.keras.layers.Conv2D(32, 4, strides=2, activation='relu'),
+      tf.keras.layers.Conv2D(32, 4, strides=2,  padding='valid', activation='relu'),
       tf.keras.layers.MaxPool2D(2, 1),
       tf.keras.layers.Flatten(),
       tf.keras.layers.Dense(32, activation='relu'),
       tf.keras.layers.Dense(10)
   ])
 
-  # Instantiate the optimizer
   if FLAGS.dpsgd:
-    opt = DPGradientDescentGaussianOptimizer(
+    optimizer = DPGradientDescentGaussianOptimizer(
         l2_norm_clip=FLAGS.l2_norm_clip,
         noise_multiplier=FLAGS.noise_multiplier,
         num_microbatches=FLAGS.microbatches,
         learning_rate=FLAGS.learning_rate)
+    # Compute vector of per-example loss rather than its mean over a minibatch.
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True, reduction=tf.compat.v1.losses.Reduction.NONE)
   else:
-    opt = tf.optimizers.SGD(learning_rate=FLAGS.learning_rate)
+    optimizer = GradientDescentOptimizer(learning_rate=FLAGS.learning_rate)
+    loss = tf.keras.losses.CategoricalCrossentropy(from_logits=True)
 
-  # Training loop.
-  steps_per_epoch = 60000 // FLAGS.batch_size
-  for epoch in range(FLAGS.epochs):
-    # Train the model for one epoch.
-    for (_, (images, labels)) in enumerate(dataset.take(-1)):
-      with tf.GradientTape(persistent=True) as gradient_tape:
-        # This dummy call is needed to obtain the var list.
-        logits = mnist_model(images, training=True)
-        var_list = mnist_model.trainable_variables
+  # Compile model with Keras
+  model.compile(optimizer=optimizer, loss=loss, metrics=['accuracy'])
 
-        def loss_fn():
-          logits = mnist_model(images, training=True)
-          loss = tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits)
-          # If training without privacy, the loss is a scalar not a vector.
-          if not FLAGS.dpsgd:
-            loss = tf.reduce_mean(loss)
-          return loss
-
-        if FLAGS.dpsgd:
-          ##### minimize
-          grads_and_vars = opt.compute_gradients(loss_fn, var_list, gradient_tape=gradient_tape)
-          opt.apply_gradients(grads_and_vars)
-          #####
-        else:
-          grads = gradient_tape.gradient(loss_fn(), var_list)
-          opt.apply_gradients(zip(grads, var_list))
-      
-    # Evaluate the model and print results
-    for (_, (images, labels)) in enumerate(eval_dataset.take(-1)):
-      logits = mnist_model(images, training=False)
-      correct_preds = tf.equal(tf.argmax(logits, axis=1), labels)
-    test_accuracy = np.mean(correct_preds.numpy())
-    print('Test accuracy after epoch {:d} is: {:.3f}'.format(epoch, test_accuracy))
-
-    # Compute the privacy budget expended so far.
-    if FLAGS.dpsgd:
-      eps = compute_epsilon((epoch + 1) * steps_per_epoch)
-      print('For delta = {}, the current epsilon is: {:.2f}'.format(delta, eps))
-    else:
-      print('Trained with vanilla non-private SGD optimizer')
+  # Train model with Keras
+  eps_callback = EpsilonPrintingCallback()
+  fit_history = model.fit(train_data, train_labels, epochs=FLAGS.epochs, validation_data=(test_data, test_labels), batch_size=FLAGS.batch_size, callbacks=[eps_callback])
+  eps_history = eps_callback.eps_history
+  val_acc_history = fit_history.history['val_accuracy']
+  with open('delta_{}_lr_{}.txt'.format(delta, FLAGS.learning_rate), 'w') as f:
+        f.write('eps: {}\n'.format(eps_history))
+        f.write('validation acc: {}\n'.format(val_acc_history))
 
 if __name__ == '__main__':
   app.run(main)
